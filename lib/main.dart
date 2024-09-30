94ce3ac411e9cfcf25f83a460ed7a4a41b23f3b0
Junior Hirwa <iamhirwejr@gmail.com>
2024-09-30 23:49:40 +0200

    feat: update main

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flearno/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        title: 'Flearnoo', debugShowCheckedModeBanner: false, home: Home());
+  }
+}
