240fd087a8cae0873ad815e32396afa75efd95c3
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-01 07:54:08 +0200

    feat: working with online images

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -13,7 +13,16 @@ class HomePage extends StatelessWidget {
           decoration: BoxDecoration(border: Border.all(color: Colors.black)),
           child:
               Image.asset("assets/images/placeholder.jpg", fit: BoxFit.cover),
-        ), 
+        ),
+        Container(
+          height: 300,
+          width: 400,
+          decoration: BoxDecoration(border: Border.all(color: Colors.green)),
+          child: Image.network(
+            "https://images.pexels.com/photos/1670977/pexels-photo-1670977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            fit: BoxFit.cover,
+          ),
+        )
       ],
     );
   }
