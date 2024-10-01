6272dc362188e6a41e376b0cede6683e0082ee2d
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-01 07:33:16 +0200

    feat: add pages and activate navigation

--- lib/bottom_navigation.dart
+++ lib/bottom_navigation.dart
@@ -11,47 +11,64 @@ class BottomNavigation extends StatefulWidget {
 class _BottomNavigationState extends State<BottomNavigation> {
   int _selectedIndex = 0;
 
-  final List<Widget> _pages  = [
-    Container(color: Colors.red),
-    Container(color: Colors.green),
-    Container(color: Colors.blue),
-    Container(color: Colors.orange),
-    Container(color: Colors.purple),
+  final List<Widget> _pages = [
+    Container(color: Colors.red[100]),
+    Container(color: Colors.green[100]),
+    Container(color: Colors.blue[100]),
+    Container(color: Colors.orange[100]),
+    Container(color: Colors.purple[100]),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      currentIndex: _selectedIndex,
-      onTap: (index) {
-        setState(() {
-          _selectedIndex = index;
-        });
-        print("index: ${_selectedIndex}");
-      },
-      items: [
-        BottomNavigationBarItem(
-            label: "",
-            icon: SvgPicture.asset("assets/vectors/home-outline.svg")),
-        BottomNavigationBarItem(
-            label: "",
-            icon: SvgPicture.asset("assets/vectors/search-outline.svg")),
-        BottomNavigationBarItem(
-            label: "",
-            icon: SvgPicture.asset("assets/vectors/add-square-outline.svg")),
-        BottomNavigationBarItem(
-            label: "",
-            icon: SvgPicture.asset("assets/vectors/video-play-outline.svg")),
-        BottomNavigationBarItem(
-            label: "",
-            icon: Container(
-              width: 30,
-              height: 30,
-              padding: const EdgeInsets.all(3),
-              child: Icon(Icons.account_circle, color: Colors.grey[600]),
-            ))
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flearnoo"),
+        backgroundColor: Colors.blueAccent[100],
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              label: "",
+              icon: SvgPicture.asset(_selectedIndex == 0
+                  ? "assets/vectors/home-bold.svg"
+                  : "assets/vectors/home-outline.svg")),
+          BottomNavigationBarItem(
+              label: "",
+              icon: SvgPicture.asset(_selectedIndex == 1
+                  ? "assets/vectors/search-bold.svg"
+                  : "assets/vectors/search-outline.svg")),
+          BottomNavigationBarItem(
+              label: "",
+              icon: SvgPicture.asset(_selectedIndex == 2
+                  ? "assets/vectors/add-square-bold.svg"
+                  : "assets/vectors/add-square-outline.svg")),
+          BottomNavigationBarItem(
+              label: "",
+              icon: SvgPicture.asset(_selectedIndex == 3
+                  ? "assets/vectors/video-play-bold.svg"
+                  : "assets/vectors/video-play-outline.svg")),
+          BottomNavigationBarItem(
+              label: "",
+              icon: Container(
+                  width: 30,
+                  height: 30,
+                  padding: const EdgeInsets.all(3),
+                  child: _selectedIndex == 4
+                      ? Icon(Icons.account_circle, color: Colors.grey[800])
+                      : Icon(Icons.account_circle, color: Colors.grey[600])))
+        ],
+      ),
     );
   }
 }
